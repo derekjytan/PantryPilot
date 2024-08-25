8a9cba7a33c0192b9bba54b5e3735e47b96d441f
derektan12 <derektan101@gmail.com>
2024-08-25 03:01:07 -0400

    fixing backend deploy

--- server/server.js
+++ server/server.js
@@ -11,22 +11,15 @@ dotenv.config({ path: './.env' });
 const app = express(); // Express 
 const port = process.env.PORT || 8000; 
 
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString('utf8')
+);
+
 // Initialize Firebase Admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert({
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
-  }),
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_DB_URL
-});
+})
 
 // Getting a reference to the Firestore database
 const db = admin.firestore();
